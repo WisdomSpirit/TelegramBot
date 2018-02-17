26f7608a889db833bfa6a26d98cf43da29331ea0
WisdomSpirit <eyeofstorm63@gmail.com>
2018-02-17 21:31:04 +0500

    first commit - just a structure with some kind of interaction code

---
+++ src/main/scala/commands/General.scala
@@ -0,0 +1,20 @@
+package commands
+
+import scala.collection.mutable.ListBuffer
+
+object General {
+  var POLLS = ListBuffer()
+
+  def createPoll(){}
+
+  def pollList(){}
+
+  def deletePoll(){}
+
+  def startPoll(){}
+
+  def stopPoll(){}
+
+  def pollResult(){}
+
+}
