23e44bd85c510ed4097c4ffc645e10b74a6714b0
FoOkySNick <eyeofstorm63@gmail.com>
2018-03-17 22:55:55 +0500

    Update TestGeneralCommands.scala

--- src/test/scala/TestGeneralCommands.scala
+++ src/test/scala/TestGeneralCommands.scala
@@ -2,5 +2,5 @@ import commands.General._
 import org.scalatest.FlatSpec
 
 class TestGeneralCommands extends FlatSpec{
-  assertResult("Your poll wasn't created! Please try again later!"){createPoll(List[String]())}
+  assertResult("Your poll wasn't created! Please try again later!"){createPoll(Try(List[String]()))}
 }
