f31409e43995ba45e234c536f71429305a061691
FoOkySNick <eyeofstorm63@gmail.com>
2018-03-31 11:12:02 +0500

    Add files via upload

--- src/test/scala/TestGeneralCommand.scala
+++ src/test/scala/TestGeneralCommand.scala
@@ -1,26 +1,25 @@
-import Repository._
 import commands.General._
+import Repository._
 import org.scalatest.FlatSpec
-
 import scala.util.Try
 
 class TestGeneralCommand extends FlatSpec{
   assertResult("Can You see the list of Your polls? I can't too. But they exists."){pollList()}
   assertResult("can't create a poll"){createPoll(Try(List[String]()))}
-  assertResult("0") {createPoll(Try(List[String]("newpoll", "yes")))}
-  assertResult("1") {createPoll(Try(List[String]("newpoll", "no")))}
+  assertResult("0") {createPoll(Try(List[String]("newpoll0", "yes")))}
+  assertResult("1") {createPoll(Try(List[String]("newpoll1", "no")))}
   assertResult("can't create a poll") {createPoll(Try(List[String]("newpoll", "ys")))}
-  assertResult("2"){createPoll(Try(List[String]("newpoll", "yes", "afterstop")))}
-  assertResult("3"){createPoll(Try(List[String]("newpoll", "yes", "continuous")))}
+  assertResult("2"){createPoll(Try(List[String]("newpoll2", "yes", "afterstop")))}
+  assertResult("3"){createPoll(Try(List[String]("newpoll3", "yes", "continuous")))}
   assertResult("can't create a poll"){createPoll(Try(List[String]("newpoll", "yes", "aftersop")))}
-  assertResult("4"){createPoll(Try(List[String]("newpoll", "yes", "continuous", "23:12:04 12:45:12")))}
+  assertResult("4"){createPoll(Try(List[String]("newpoll4", "yes", "continuous", "23:12:04 12:45:12")))}
   assertResult("can't create a poll"){createPoll(Try(List[String]("newpoll", "yes", "continuous", "23:12:04 12::10")))}
   assertResult("5")
-    {createPoll(Try(List[String]("newpoll", "no", "continuous", "23:12:04 12:45:12", "23:12:54 12:45:32")))}
+  {createPoll(Try(List[String]("newpoll5", "no", "continuous", "23:12:04 12:45:12", "23:12:54 12:45:32")))}
   assertResult("can't create a poll"){createPoll(Try(List[String]("newpoll", "yes", "continuous", "23:12:04 12:sd:10")))}
   assertResult("can't create a poll"){createPoll(Try(List[String]("newpoll", "yes", "continuous", "23:12:04:23 12::10")))}
   assertResult("can't create a poll")
-    {createPoll(Try(List[String]("newpoll", "yes", "afterstop", "23:12:04 12:45:12", "23:2:54 12:45:32")))}
+  {createPoll(Try(List[String]("newpoll", "yes", "afterstop", "23:12:04 12:45:12", "23:2:54 12:45:32")))}
 
   assertResult(6){AllPolls.getAll.count(e => true)}
 
@@ -29,7 +28,7 @@ class TestGeneralCommand extends FlatSpec{
   assert(AllPolls.get("1").isSuccess)
   assertResult("Exterminate! Exterminate! Exterminate!"){deletePoll(Try(List[String]("1")))}
   assert(AllPolls.get("1").isFailure)
-  assertResult("Can't delete Your Poll, cuz there's no such one!"){deletePoll(Try(List[String]("")))}
+  assertResult("Can't delete Your Poll, cuz there's no such one!"){deletePoll(Try(List[String]("1")))}
 
   assert(RunPolls.getAll.isEmpty)
   assertResult("Your poll was just started, look for feedback!"){startPoll(Try(List[String]("0")))}
@@ -42,5 +41,4 @@ class TestGeneralCommand extends FlatSpec{
   assertResult("Cant't stop Your Poll, cuz it's not run!"){stopPoll(Try(List[String]("0")))}
 
 //  print(pollResult(Try(List[String]("3"))))
-
 }
---
+++ src/test/scala/TestParsing.scala
@@ -0,0 +1,45 @@
+import Repository._
+import commands.General._
+import interaction.Reader._
+import org.scalatest.FlatSpec
+
+
+class TestParsing extends FlatSpec {
+
+  assertResult("Can You see the list of Your polls? I can't too. But they exists.")
+  {createCommands("/list").get.execute()}
+
+  assert(createCommands("/create_poll (fghjkhk((akjdj)))").isSuccess)
+  assertResult("0"){createCommands("/create_poll (name0)").get.execute()}
+  assertResult("1"){createCommands("/create_poll (name ((1))) (yes)").get.execute()}
+  assertResult("2"){createCommands("/create_poll (n a m e 2) (no) (afterstop)").get.execute()}
+  assertResult("3"){createCommands("/create_poll (na((me)) 3) (yes) (continuous) (23:23:23 23:23:23)")
+    .get.execute()}
+  assertResult("4"){createCommands("/create_poll (name4) (yes) (continuous) (23:23:23 23:23:23)" +
+    " (25:25:25 25:25:25)").get.execute()}
+
+  print(pollList())
+
+  assertResult("Unrecognised command! Say what!?")(createCommands("/creat_pol (qwerty)").get.execute())
+
+  assertResult("Exterminate! Exterminate! Exterminate!"){createCommands("/delete_poll (0)").get.execute()}
+  assert(AllPolls.get("0").isFailure)
+  assertResult("Can't delete Your Poll, cuz there's no such one!"){createCommands("/delete_poll ").get.execute()}
+  assertResult("Can't delete Your Poll, cuz there's no such one!"){createCommands("/delete_poll ()").get.execute()}
+  assertResult("Can't delete Your Poll, cuz there's no such one!"){createCommands("/delete_poll (5)").get.execute()}
+  assertResult("Can't delete Your Poll, cuz there's no such one!"){createCommands("/delete_poll (d)").get.execute()}
+
+  assertResult("Your poll was just started, look for feedback!"){createCommands("/start_poll (1)").get.execute()}
+  assert(RunPolls.get("1").isSuccess)
+  assertResult("Can't start your Poll, cuz there's no such one!")
+  {createCommands("/start_poll ()").get.execute()}
+  assertResult("Can't start your Poll, cuz there's no such one!")
+  {createCommands("/start_poll (d)").get.execute()}
+  assertResult("Can't start your Poll, cuz there's no such one!")
+  {createCommands("/start_poll (12)").get.execute()}
+
+  assertResult("Your poll was just finished, that was a great poll!") {createCommands("/stop_poll (1)").get.execute()}
+  assert(RunPolls.get("1").isFailure)
+  assertResult("Cant't stop Your Poll, cuz it's not run!") {createCommands("/stop_poll (12)").get.execute()}
+  assertResult("Cant't stop Your Poll, cuz it's not run!") {createCommands("/stop_poll (f)").get.execute()}
+}
